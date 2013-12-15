dc21c49ce76428f0ec982266eb3541ec4dff4c27
Steven Luo <steven@steven676.net>
2013-12-15 05:17:50 -0800

    common_mini_tablet_wifionly.mk: don't try to install removed BlueZ config file
    Change-Id: I91ac1dd34c9c048cda7d6b5a81ee22b2db852b49

--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -13,10 +13,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=Teleport.ogg \
     ro.config.alarm_alert=Light.ogg
 
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
-
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
         vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
